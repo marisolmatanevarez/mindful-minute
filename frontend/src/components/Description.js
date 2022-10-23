55dd8bcf4b0a86f58fb56cef5682eb4823ca83e3
mm976 <99553893+marisolmatanevarez@users.noreply.github.com>
2022-10-23 12:46:26 -0400

    added description

--- frontend/src/components/Description.js
+++ frontend/src/components/Description.js
@@ -2,7 +2,7 @@ function Description() {
     return (
         // {/*className="card"*/}
         <p>
-            Mindful Moments is ..........................................................
+            Mindful Moments is a web application that sends you a daily meme, inspirational quote, or self-care message.
         </p>
     );
 }
