66b2422aeeec5f11e4ab70cb4998d13f25de998d
mm976 <99553893+marisolmatanevarez@users.noreply.github.com>
2022-10-22 22:24:24 -0400

    Back to normal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,12 @@ import Signup from './components/Signup';
 
 function App() {
   return (
-      <div className="backdrop">
+      <div className="backdrop" style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh'
+      }}>
         <Signup />
       </div>
   );
